5f1d372db949c4d2d370804130b828b0b7940822
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-03 19:23:24 +0900

    add burger np install but not found error

--- react-i18next-sample/src/App.tsx
+++ react-i18next-sample/src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useTranslation } from 'react-i18next';
+ 
+const AppPage = () => {
+  const { i18n } = useTranslation();
+ 
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={() => changeLanguage('ja')}>ja</button>
+        <button onClick={() => changeLanguage('en')}>en</button>
+      </div>
+      <TestComponent />
     </div>
+  );
+}
+ 
+function App() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <AppPage />
+    </Suspense>
   );
 }
 
